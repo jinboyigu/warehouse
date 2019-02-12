117c934eb49bba147ff32ea0f5e305c57b32c379
jyhuang <326127285@qq.com>
2019-02-12 15:59:38 +0800

    feat: add storage

--- src/router.js
+++ src/router.js
@@ -22,5 +22,11 @@ export default new Router({
       component: () =>
         import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/storage',
+      name: 'storage',
+      component: () =>
+        import(/* webpackChunkName: "storage" */ './views/storage'),
+    },
   ],
 });
---
+++ src/views/storage/index.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <el-col :offset="2" :span="20">
+      <h4 style="text-align: left">标题</h4>
+      <el-table :data="tableData" :stripe="true" size="medium">
+        <el-table-column
+          v-for="column in tableProps"
+          :key="column.value"
+          :prop="column.value"
+          :label="column.label"
+        >
+          <template slot-scope="{ row }">
+            <el-input
+              v-model="row[getInputModelKey(column.value)]"
+              v-if="row.editing"
+            />
+            <div v-else>{{ row[column.value] }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column align="right">
+          <template slot="header">
+            <el-input
+              v-model="keyword"
+              size="mini"
+              placeholder="输入关键字搜索"
+            />
+          </template>
+          <div style="text-align: center" slot-scope="target">
+            <el-button
+              type="primary"
+              :icon="
+                tableData[target.$index].editing
+                  ? 'el-icon-check'
+                  : 'el-icon-edit'
+              "
+              circle
+              @click="toggleEdit(target)"
+            ></el-button>
+            <el-button
+              icon="el-icon-delete"
+              circle
+              @click="handleDelete(target)"
+            ></el-button>
+            <el-button
+              icon="el-icon-plus"
+              circle
+              @click="handleAdd(target)"
+            ></el-button>
+          </div>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </div>
+</template>
+
+<script>
+const inputModelSymbol = Symbol('updatedInputModelKey');
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          header: '123',
+          header1: '1231',
+          header2: '1232',
+          header3: '1233',
+        },
+        {
+          header: '123',
+          header1: '1231',
+          header2: '1232',
+          header3: '1233',
+        },
+      ],
+      tableProps: [
+        { label: '表头', value: 'header' },
+        { label: '表头1', value: 'header1' },
+        { label: '表头2', value: 'header2' },
+        { label: '表头3', value: 'header3' },
+      ],
+      keyword: '',
+    };
+  },
+  watch: {
+    tableData: {
+      immediate: true,
+      handler(_old, _new) {
+        (_new ? _new : _old).forEach(o => {
+          if (!o.hasOwnProperty(inputModelSymbol)) {
+            Object.keys(o).forEach(key => {
+              this.$set(o, this.getInputModelKey(key), '');
+            });
+            this.$set(o, 'editing', o.editing || false);
+            o[inputModelSymbol] = true;
+          }
+        });
+      },
+    },
+  },
+  methods: {
+    toggleEdit(target) {
+      const data = this.tableData[target.$index];
+      const editing = data.editing;
+      if (editing) {
+        // TODO check data
+      }
+      this.tableProps.forEach(o => {
+        const _key = o.value;
+        if (editing) {
+          data[_key] = data[this.getInputModelKey(_key)];
+        } else {
+          data[this.getInputModelKey(_key)] = data[_key];
+        }
+      });
+      data.editing = !editing;
+    },
+    getInputModelKey(key) {
+      return `_${key}InputModel`;
+    },
+    handleAdd(target) {
+      let newData = { editing: true };
+      this.tableProps.forEach(o => {
+        newData[o.value] = '';
+      });
+      this.tableData.splice(target.$index + 1, 0, newData);
+    },
+    handleDelete(target) {
+      this.tableData.splice(target.$index, 1);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less"></style>
